a5370bffa66570d6c566a765b583b08cb1b764f0
amirvein <amir.aha73566@gmail.com>
2024-01-29 16:34:11 +0330

    gg

---
+++ src/components/productDetail.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex w-full h-full justify-center items-center">
+        <div class="flex flex-row w-full h-full">
+            <div class="w-2/3 h-full bg-primary-100 p-5">{{ desc }}</div>
+            <div class="w-1/3 bg-primary-300 h-full p-5">{{ title }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        desc: String,
+    }
+}
+</script>
+
--- src/views/product.vue
+++ src/views/product.vue
@@ -3,31 +3,15 @@
   <div class="flex flex-col h-full w-full justify-center items-center mt-16">
     <div class="flex flex-row-reverse w-[68%] min-w[1000px]">
       <div class="w-[38%] h-full flex flex-col space-y-6">
-        <Carousel
-          id="gallery"
-          :items-to-show="1"
-          :wrap-around="false"
-          v-model="currentSlide"
-        >
+        <Carousel id="gallery" :items-to-show="1" :wrap-around="false" v-model="currentSlide">
           <Slide v-for="slide in imageUrl()" :key="slide">
-            <div
-              class="carousel__item relative max-w-xs overflow-hidden bg-cover bg-no-repeat"
-            >
-              <img
-                :src="'/product/' + slide"
-                alt=""
-                class="max-w-xs transition duration-300 ease-in-out hover:scale-110"
-              />
+            <div class="carousel__item relative max-w-xs overflow-hidden bg-cover bg-no-repeat">
+              <img :src="'/product/' + slide" alt=""
+                class="max-w-xs transition duration-300 ease-in-out hover:scale-110" />
             </div>
           </Slide>
         </Carousel>
-        <Carousel
-          id="thumbnails"
-          :items-to-show="3"
-          :wrap-around="true"
-          v-model="currentSlide"
-          ref="carousel"
-        >
+        <Carousel id="thumbnails" :items-to-show="3" :wrap-around="true" v-model="currentSlide" ref="carousel">
           <Slide v-for="slide in imageUrl()" :key="slide">
             <div class="carousel__item" @click="slideTo(slide - 1)">
               <img :src="'/product/' + slide" alt="" />
@@ -36,17 +20,14 @@
         </Carousel>
       </div>
       <div
-        class="w-[70%] justify-between h-full items-end flex flex-col shadow-md rounded-lg shadow-[lightgray] px-8 pt-7 pb-4 space-y-6 text-right"
-      >
+        class="w-[70%] justify-between h-full items-end flex flex-col shadow-md rounded-lg shadow-[lightgray] px-8 pt-7 pb-4 space-y-6 text-right">
         <p class="font-bold pname text-lg">{{ product.Name }}</p>
         <div class="font-bold text-blue text-[21px] flex flex-row space-x-1">
           <p class="">تومان</p>
           <p class="">{{ Intl.NumberFormat("fa").format(product.Price) }}</p>
         </div>
         <p class="text-sm font-bold">ویزگی ها</p>
-        <div
-          class="flex flex-row space-x-3 justify-center items-center font-bold text-sm"
-        >
+        <div class="flex flex-row space-x-3 justify-center items-center font-bold text-sm">
           <div class="flex flex-row space-x-1 text-blue">
             <p>گیگابایت</p>
             <p>{{ product.InternalMemory }}</p>
@@ -54,9 +35,7 @@
           <p>: حافظه داخلی</p>
           <font-awesome-icon icon="fa-solid fa-angle-left" />
         </div>
-        <div
-          class="flex flex-row space-x-3 justify-center items-center font-bold text-sm"
-        >
+        <div class="flex flex-row space-x-3 justify-center items-center font-bold text-sm">
           <div class="flex flex-row space-x-1 text-blue">
             <p>گیگابایت</p>
             <p>{{ product.Ram }}</p>
@@ -64,9 +43,7 @@
           <p>: Ram مقدار</p>
           <font-awesome-icon icon="fa-solid fa-angle-left" />
         </div>
-        <div
-          class="flex flex-row space-x-3 justify-center items-center font-bold text-sm"
-        >
+        <div class="flex flex-row space-x-3 justify-center items-center font-bold text-sm">
           <div class="flex flex-row space-x-1 text-blue">
             <p>اینچ</p>
             <p>{{ product.Screen }}</p>
@@ -74,18 +51,14 @@
           <p>: بازه‌ی اندازه صفحه نمایش</p>
           <font-awesome-icon icon="fa-solid fa-angle-left" />
         </div>
-        <div
-          class="flex flex-row space-x-3 justify-center items-center font-bold text-sm"
-        >
+        <div class="flex flex-row space-x-3 justify-center items-center font-bold text-sm">
           <div class="flex flex-row space-x-1 text-blue">
             <p>{{ product.Networks }}</p>
           </div>
           <p>: شبکه های ارتباطی</p>
           <font-awesome-icon icon="fa-solid fa-angle-left" />
         </div>
-        <div
-          class="flex flex-row space-x-3 justify-center items-center font-bold text-sm"
-        >
+        <div class="flex flex-row space-x-3 justify-center items-center font-bold text-sm">
           <div class="flex flex-row space-x-1 text-blue">
             <p>مگاپیکسل</p>
             <p>{{ product.PictureRes }}</p>
@@ -96,73 +69,36 @@
         <div class="flex flex-col space-y-2">
           <p>رنگ ها</p>
           <div class="productColor flex flex-row space-x-2">
-            <div
-              class="w-[24px] h-[24px] bg-mobileBlack rounded-full shadow-md"
-              v-if="colorList().includes('Black')"
-            ></div>
-            <div
-              class="w-[24px] h-[24px] bg-mobileOlive rounded-full shadow-md"
-              v-if="colorList().includes('Olive')"
-            ></div>
-            <div
-              class="w-[24px] h-[24px] bg-mobilePurple rounded-full shadow-md"
-              v-if="colorList().includes('Purple')"
-            ></div>
-            <div
-              class="w-[24px] h-[24px] bg-mobileWhite rounded-full shadow-md"
-              v-if="colorList().includes('White')"
-            ></div>
-            <div
-              class="w-[24px] h-[24px] bg-mobileCream rounded-full shadow-md"
-              v-if="colorList().includes('Cream')"
-            ></div>
-            <div
-              class="w-[24px] h-[24px] bg-gray rounded-full shadow-md"
-              v-if="colorList().includes('Gray')"
-            ></div>
-            <div
-              class="w-[24px] h-[24px] bg-Sierrablue rounded-full shadow-md"
-              v-if="colorList().includes('Sierrablue')"
-            ></div>
-            <div
-              class="w-[24px] h-[24px] bg-mobileBlue rounded-full shadow-md"
-              v-if="colorList().includes('Blue')"
-            ></div>
-            <div
-              class="w-[24px] h-[24px] bg-mobileGreen rounded-full shadow-md"
-              v-if="colorList().includes('Green')"
-            ></div>
-            <div
-              class="w-[24px] h-[24px] bg-Burgundy rounded-full shadow-md"
-              v-if="colorList().includes('Burgundy')"
-            ></div>
-            <div
-              class="w-[24px] h-[24px] bg-Beige rounded-full shadow-md"
-              v-if="colorList().includes('Beige')"
-            ></div>
-            <div
-              class="w-[24px] h-[24px] bg-brown rounded-full shadow-md"
-              v-if="colorList().includes('Brown')"
-            ></div>
-            <div
-              class="w-[24px] h-[24px] bg-lime rounded-full shadow-md"
-              v-if="colorList().includes('Lime')"
-            ></div>
-            <div
-              class="w-[24px] h-[24px] bg-silver rounded-full shadow-md"
-              v-if="colorList().includes('Silver')"
-            ></div>
+            <div class="w-[24px] h-[24px] bg-mobileBlack rounded-full shadow-md" v-if="colorList().includes('Black')">
+            </div>
+            <div class="w-[24px] h-[24px] bg-mobileOlive rounded-full shadow-md" v-if="colorList().includes('Olive')">
+            </div>
+            <div class="w-[24px] h-[24px] bg-mobilePurple rounded-full shadow-md" v-if="colorList().includes('Purple')">
+            </div>
+            <div class="w-[24px] h-[24px] bg-mobileWhite rounded-full shadow-md" v-if="colorList().includes('White')">
+            </div>
+            <div class="w-[24px] h-[24px] bg-mobileCream rounded-full shadow-md" v-if="colorList().includes('Cream')">
+            </div>
+            <div class="w-[24px] h-[24px] bg-gray rounded-full shadow-md" v-if="colorList().includes('Gray')"></div>
+            <div class="w-[24px] h-[24px] bg-Sierrablue rounded-full shadow-md" v-if="colorList().includes('Sierrablue')">
+            </div>
+            <div class="w-[24px] h-[24px] bg-mobileBlue rounded-full shadow-md" v-if="colorList().includes('Blue')"></div>
+            <div class="w-[24px] h-[24px] bg-mobileGreen rounded-full shadow-md" v-if="colorList().includes('Green')">
+            </div>
+            <div class="w-[24px] h-[24px] bg-Burgundy rounded-full shadow-md" v-if="colorList().includes('Burgundy')">
+            </div>
+            <div class="w-[24px] h-[24px] bg-Beige rounded-full shadow-md" v-if="colorList().includes('Beige')"></div>
+            <div class="w-[24px] h-[24px] bg-brown rounded-full shadow-md" v-if="colorList().includes('Brown')"></div>
+            <div class="w-[24px] h-[24px] bg-lime rounded-full shadow-md" v-if="colorList().includes('Lime')"></div>
+            <div class="w-[24px] h-[24px] bg-silver rounded-full shadow-md" v-if="colorList().includes('Silver')"></div>
           </div>
         </div>
 
         <div class="flex flex-col space-y-3">
           <label for="waranti">: گارانتی </label>
           <div>
-            <select
-              name="Waranti"
-              id="waranti"
-              class="prudoctselect cursor-pointer block outline-none border-2 rounded-3xl w-72 p-2 text-sm border-lightergray text-ultragray"
-            >
+            <select name="Waranti" id="waranti"
+              class="prudoctselect cursor-pointer block outline-none border-2 rounded-3xl w-72 p-2 text-sm border-lightergray text-ultragray">
               <option value="" disabled selected hidden>
                 یک گزینه را انتخاب کنید
               </option>
@@ -173,25 +109,17 @@
           </div>
         </div>
         <div class="flex flex-row justify-center space-x-5 items-center pt-4">
-          <button
-            class="px-6 py-3 bg-lightBlue font-semibold rounded-lg text-white"
-          >
+          <button class="px-6 py-3 bg-lightBlue font-semibold rounded-lg text-white">
             افزودن به سبد خرید
           </button>
           <div class="flex justify-center items-center">
-            <button
-              @click="counter('+')"
-              class="p-2 border-black border-2 rounded-l-3xl"
-            >
+            <button @click="counter('+')" class="p-2 border-black border-2 rounded-l-3xl">
               +
             </button>
             <p class="py-2 border-y-2 border-black w-8 text-center">
               {{ count }}
             </p>
-            <button
-              @click="counter('-')"
-              class="p-2 border-black border-2 rounded-r-3xl"
-            >
+            <button @click="counter('-')" class="p-2 border-black border-2 rounded-r-3xl">
               -
             </button>
           </div>
@@ -227,22 +155,16 @@
           </div>
         </div>
         <div class="flex flex-row-reverse gap-4">
-          <div
-            class="flex flex-row-reverse text-[15px] items-center gap-1 font-semibold hover:text-gray cursor-pointer"
-          >
+          <div class="flex flex-row-reverse text-[15px] items-center gap-1 font-semibold hover:text-gray cursor-pointer">
             <font-awesome-icon icon="fa-solid  fa-shuffle" class="" />
             <p>مقایسه</p>
           </div>
-          <div
-            class="flex flex-row-reverse text-[15px] items-center gap-1 font-semibold hover:text-gray cursor-pointer"
-          >
+          <div class="flex flex-row-reverse text-[15px] items-center gap-1 font-semibold hover:text-gray cursor-pointer">
             <font-awesome-icon icon="fa-regular fa-heart" class="" />
             <p>افزودن به علاقه مندی</p>
           </div>
         </div>
-        <div
-          class="w-full py-4 px-5 bg-lightWhite rounded-2xl flex flex-row-reverse"
-        >
+        <div class="w-full py-4 px-5 bg-lightWhite rounded-2xl flex flex-row-reverse">
           <div>10</div>
           <p class="pname">نفر در حال مشاهده این محصول هستند!</p>
         </div>
@@ -250,54 +172,36 @@
           <p></p>
           <p></p>
         </div>
-        <div
-          class="flex flex-row space-x-3 justify-center items-center font-bold text-sm"
-        >
+        <div class="flex flex-row space-x-3 justify-center items-center font-bold text-sm">
           <div class="flex flex-row space-x-1 text-blue">
             <p>{{ product.Networks }}</p>
           </div>
           <p>: شبکه های ارتباطی</p>
         </div>
-        <div
-          class="flex flex-row space-x-3 justify-center items-center font-bold text-sm"
-        >
+        <div class="flex flex-row space-x-3 justify-center items-center font-bold text-sm">
           <div class="flex flex-row space-x-1 text-blue">
             <p>{{ product.Networks }}</p>
           </div>
           <p>: دسته</p>
         </div>
-        <div
-          class="flex flex-row space-x-3 justify-center items-center font-bold text-sm"
-        >
+        <div class="flex flex-row space-x-3 justify-center items-center font-bold text-sm">
           <div class="flex flex-row space-x-1 text-blue">
             <p>{{ product.Networks }}</p>
           </div>
           <p>: برچسب</p>
         </div>
-        <div
-          class="flex flex-row space-x-3 justify-center items-center font-bold text-sm"
-        >
+        <div class="flex flex-row space-x-3 justify-center items-center font-bold text-sm">
           <div class="flex flex-row space-x-1 gap-3">
-            <font-awesome-icon
-              class="text-base text-midgray/70 cursor-pointer hover:text-midgray"
-              icon="fa-brands fa-telegram"
-            />
-            <font-awesome-icon
-              class="text-base text-midgray/70 cursor-pointer hover:text-midgray"
-              icon="fa-brands fa-linkedin-in"
-            />
-            <font-awesome-icon
-              class="text-base text-midgray/70 cursor-pointer hover:text-midgray"
-              icon="fa-brands fa-pinterest"
-            />
-            <font-awesome-icon
-              class="text-base text-midgray/70 cursor-pointer hover:text-midgray"
-              icon="fa-brands fa-twitter"
-            />
-            <font-awesome-icon
-              class="text-base text-midgray/70 cursor-pointer hover:text-midgray"
-              icon="fa-brands fa-facebook-f"
-            />
+            <font-awesome-icon class="text-base text-midgray/70 cursor-pointer hover:text-midgray"
+              icon="fa-brands fa-telegram" />
+            <font-awesome-icon class="text-base text-midgray/70 cursor-pointer hover:text-midgray"
+              icon="fa-brands fa-linkedin-in" />
+            <font-awesome-icon class="text-base text-midgray/70 cursor-pointer hover:text-midgray"
+              icon="fa-brands fa-pinterest" />
+            <font-awesome-icon class="text-base text-midgray/70 cursor-pointer hover:text-midgray"
+              icon="fa-brands fa-twitter" />
+            <font-awesome-icon class="text-base text-midgray/70 cursor-pointer hover:text-midgray"
+              icon="fa-brands fa-facebook-f" />
           </div>
           <p>: اشتراک گذاری</p>
         </div>
@@ -309,55 +213,47 @@
     <p></p>
   </div>
   <div class="flex justify-center items-center h-full {{ activeCount() }}">
-    <div
-      class="flex flex-row-reverse w-[70%] gap-8 items-center pt-7 font-bold text-lightgray"
-    >
-      <button
-        @click="ChangeActive('توضیحات'), getItem()"
-        :class="
-          countdesc === 'توضیحات'
-            ? 'text-blue border-b-2 border-blue'
-            : 'text-gray'
-        "
-        class="text-lg transition duration-300 ease-in-out cursor-pointer"
-      >
-        توضیحات
+    <div class="flex flex-row-reverse w-[70%] gap-8 items-center pt-7 font-bold text-lightgray">
+      <button @click="ChangeActive('توضیحات')" :class="countdesc === 'توضیحات'
+        ? 'text-blue border-b-2 border-blue'
+        : 'text-gray'
+        " class="text-lg transition duration-300 ease-in-out cursor-pointer">
+        <p>توضیحات</p>
       </button>
-      <button
-        @click="ChangeActive('مشخصات'), getItem()"
-        :class="
-          countdesc === 'مشخصات'
-            ? 'text-blue border-b-2 border-blue'
-            : 'text-gray'
-        "
-        class="text-lg transition duration-300 ease-in-out cursor-pointer"
-      >
+      <button @click="ChangeActive('مشخصات')" :class="countdesc === 'مشخصات'
+        ? 'text-blue border-b-2 border-blue'
+        : 'text-gray'
+        " class="text-lg transition duration-300 ease-in-out cursor-pointer">
         مشخصات کلی
       </button>
-      <button
-        @click="ChangeActive('نظرات'), getItem()"
-        :class="
-          countdesc === 'نظرات'
-            ? 'text-blue border-b-2 border-blue'
-            : 'text-gray'
-        "
-        class="text-lg transition duration-300 ease-in-out cursor-pointer"
-      >
+      <button @click="ChangeActive('نظرات')" :class="countdesc === 'نظرات'
+        ? 'text-blue border-b-2 border-blue'
+        : 'text-gray'
+        " class="text-lg transition duration-300 ease-in-out cursor-pointer">
         نظرات (0)
       </button>
-      <button
-        @click="ChangeActive('گارانتی'), getItem()"
-        :class="
-          countdesc === 'گارانتی'
-            ? 'text-blue border-b-2 border-blue'
-            : 'text-gray'
-        "
-        class="text-lg transition duration-300 ease-in-out cursor-pointer"
-      >
+      <button @click="ChangeActive('گارانتی')" :class="countdesc === 'گارانتی'
+        ? 'text-blue border-b-2 border-blue'
+        : 'text-gray'
+        " class="text-lg transition duration-300 ease-in-out cursor-pointer">
         شرایط گارانتی
       </button>
     </div>
   </div>
+  <div class="w-full h-full flex justify-center items-center pt-5">
+    <div class=" w-[70%] h-full text-right text-lg text-black" v-if="countdesc === 'توضیحات'">{{ product.Description }}
+    </div>
+    <div class="w-[70%] h-full text-right text-lg text-black" v-if="countdesc === 'مشخصات'">
+      <productDetail title="مدل‌‌ نامبرهای این محصول" :desc=product.ModelNumbers class="w-full h-full" />
+    </div>
+    <div class="w-[70%] h-full text-right text-lg text-black" v-if="countdesc === 'نظرات'">
+      <productDetail title="مدل‌‌ نامبرهای این محصول" :desc=product.ModelNumbers class="w-full h-full" />
+    </div>
+    <div class="w-[70%] h-full text-right text-lg text-black" v-if="countdesc === 'گارانتی'">
+      <productDetail title="مدل‌‌ نامبرهای این محصول" :desc=product.ModelNumbers class="w-full h-full" />
+    </div>
+  </div>
+
   <footerCmponent />
 </template>
 
@@ -380,7 +276,7 @@ export default defineComponent({
     currentSlide: 0,
     product: [],
     count: 1,
-    countdesc: 1,
+    countdesc: "توضیحات",
   }),
   mounted() {
     this.getProduct();
@@ -426,13 +322,13 @@ export default defineComponent({
     },
     ChangeActive(number) {
       this.countdesc = number;
-      console.log(this.countdesc);
     },
   },
 });
 </script>
 
 <script setup>
+import productDetail from "@/components/productDetail.vue";
 import headerComponent from "../components/headerComponent.vue";
 import footerCmponent from "@/components/footerCmponent.vue";
 </script>
